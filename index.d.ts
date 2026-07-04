845ad7f87a95113176a8517bfd54017191487ea3
HARINI01-vaskl <harihasi2006@gmail.com>
2026-07-05 00:06:25 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,15 @@
-// Copyright 2017 Lovell Fuller and others.
-// SPDX-License-Identifier: Apache-2.0
+type Promisable<T> = T | Promise<T>;
 
-export const GLIBC: 'glibc';
-export const MUSL: 'musl';
+declare namespace escalade {
+	export type Callback = (
+		directory: string,
+		files: string[],
+	) => Promisable<string | false | void>;
+}
 
-export function family(): Promise<string | null>;
-export function familySync(): string | null;
+declare function escalade(
+	directory: string,
+	callback: escalade.Callback,
+): Promise<string | void>;
 
-export function isNonGlibcLinux(): Promise<boolean>;
-export function isNonGlibcLinuxSync(): boolean;
-
-export function version(): Promise<string | null>;
-export function versionSync(): string | null;
+export = escalade;
